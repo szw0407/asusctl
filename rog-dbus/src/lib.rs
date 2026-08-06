7f4a8ec9bd9fc1c2a8e38f63449c686abc417ad2
Ahmed Yaseen <yaseen@ghoul.dev>
2026-08-07 00:07:57 +0500

    refactor(rog-dbus): remove unused has_iface helpers

--- rog-dbus/src/lib.rs
+++ rog-dbus/src/lib.rs
@@ -28,34 +28,6 @@ pub fn list_iface_blocking() -> Result<Vec<String>, Box<dyn std::error::Error>> 
     Ok(ifaces)
 }
 
-pub fn has_iface_blocking(iface: &str) -> Result<bool, Box<dyn std::error::Error>> {
-    let conn = zbus::blocking::Connection::system()?;
-    let f = zbus::blocking::fdo::ObjectManagerProxy::new(&conn, "xyz.ljones.Asusd", "/")?;
-    let interfaces = f.get_managed_objects()?;
-    for v in interfaces.iter() {
-        for k in v.1.keys() {
-            if k.as_str() == iface {
-                return Ok(true);
-            }
-        }
-    }
-    Ok(false)
-}
-
-pub async fn has_iface(iface: &str) -> Result<bool, Box<dyn std::error::Error>> {
-    let conn = zbus::Connection::system().await?;
-    let f = zbus::fdo::ObjectManagerProxy::new(&conn, "xyz.ljones.Asusd", "/").await?;
-    let interfaces = f.get_managed_objects().await?;
-    for v in interfaces.iter() {
-        for k in v.1.keys() {
-            if k.as_str() == iface {
-                return Ok(true);
-            }
-        }
-    }
-    Ok(false)
-}
-
 pub async fn find_iface_async<T>(iface_name: &str) -> Result<Vec<T>, Box<dyn std::error::Error>>
 where
     T: ProxyImpl<'static> + From<zbus::Proxy<'static>>,
